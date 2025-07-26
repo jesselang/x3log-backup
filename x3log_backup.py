0ce180dbe84e92d16ac6b8c975b81ad39f7810f4
Jesse Lang <jesse@jesselang.com>
2025-07-26 12:33:19 -0500

    Get token from email/password. Update dependencies.

--- x3log_backup.py
+++ x3log_backup.py
@@ -6,27 +6,50 @@ import sys
 import requests
 
 URL_BASE = "https://api.rbtlog.com"
-TOKEN = os.environ.get("X3LOG_TOKEN", "")
 
-def get(url):
+def get_token(url, email, password):
+    resp = requests.post(url,
+        headers={"Accept": "application/json"},
+        json={"email": email, "password": password},
+    )
+    resp.raise_for_status()
+    return resp.json()["data"]["attributes"]["token"]
+
+def get(url, token):
     resp = requests.get(url, headers={
         "Accept": "application/json",
-        "Authorization": "Bearer "+ TOKEN,
+        "Authorization": f"Bearer {token}",
     })
     resp.raise_for_status()
     return resp.json()
 
-if __name__ == "__main__":
-    if TOKEN == "":
-        print("error: X3LOG_TOKEN not defined", file=sys.stderr)
-        sys.exit(1)
 
-    workouts = get(f"{URL_BASE}/workouts?include=exercises")
+if __name__ == "__main__":
+    token = os.environ.get("X3LOG_TOKEN", "")
+    if token == "":
+        print("warning: X3LOG_TOKEN not defined", file=sys.stderr)
+        email = os.environ.get("X3LOG_EMAIL", "")
+        password = os.environ.get("X3LOG_PASSWORD", "")
+
+        if email == "" or password == "":
+            print("error: X3LOG_EMAIL or X3LOG_PASSWORD not defined",
+                  file=sys.stderr)
+            sys.exit(1)
+
+        token = get_token(f"{URL_BASE}/auth/login", email, password)
+
+        if token == "":
+            print("error: could not obtain token from email/password",
+                  file=sys.stderr)
+            sys.exit(1)
+
+
+    workouts = get(f"{URL_BASE}/workouts?include=exercises", token)
 
     print(json.dumps({
-        "bands": get(f"{URL_BASE}/bands")["data"],
-        "movements": get(f"{URL_BASE}/movements")["data"],
-        "templates": get(f"{URL_BASE}/templates")["data"],
+        "bands": get(f"{URL_BASE}/bands", token)["data"],
+        "movements": get(f"{URL_BASE}/movements", token)["data"],
+        "templates": get(f"{URL_BASE}/templates", token)["data"],
         "workouts": workouts["data"],
         "exercises": workouts["included"],
     }))
